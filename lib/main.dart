46ab076836bcfb7aea2ccfb1b2c24a514ce80248
Aaryanajith <aaryanajith45@gmail.com>
2023-04-23 20:35:59 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hms/screens/signup.dart';
+import 'package:splashscreen/splashscreen.dart';
 // import 'screens/login.dart';
 
 
@@ -12,10 +13,21 @@ class MyApp extends StatelessWidget{
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: SignUpScreen(),
+      home: SplashScreen(
+        seconds: 4,
+        navigateAfterSeconds: const SignUpScreen(),
+        image: Image.asset('assets/splash_screen/logo.png'),
+        photoSize: 100.0,
+        gradientBackground: const LinearGradient(
+            begin: Alignment(0, 370),
+            end: Alignment(430, 0),
+            colors: [
+              Color.fromRGBO(216, 75, 40, 0.788),
+              Color.fromRGBO(255, 129, 13, 0.7900000214576721)
+            ]),
+        
       ),
     );
   }
