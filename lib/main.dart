b9ac0ed3aa6fc0b40583fd69c1dcb65e3e48b55b
Aaryanajith <aaryanajith45@gmail.com>
2023-04-29 22:58:04 +0530

    Fixed scrollability

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: IntroScreen(),
@@ -24,32 +24,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// class MyStatefulWidget extends StatefulWidget {
-//   @override
-//   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-// }
-
-// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Sample Code'),
-//         ),
-//         bottomNavigationBar: NavigationBarTheme(
-//             data: NavigationBarThemeData(
-//               indicatorColor: Color.fromARGB(255, 235, 94, 12),
-//               labelTextStyle: MaterialStateProperty.all(
-//                 TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-//               ),
-//             ),
-//             child: NavigationBar(
-//               height: 60,
-//               destinations: [
-//                 NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-//                 NavigationDestination(
-//                     icon: Icon(Icons.person), label: 'Account'),
-//               ],
-//             )));
-//   }
-// }
